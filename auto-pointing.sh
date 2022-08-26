1bcb9a4dc457d13964c6bad3bd31df1835cdf079
bracoli <108661415+bracoli@users.noreply.github.com>
2022-08-26 10:33:30 +0800

    Update auto-pointing.sh

--- auto-pointing.sh
+++ auto-pointing.sh
@@ -20,11 +20,11 @@ echo "$SUB_DOMAIN" >> /etc/v2ray/domain
 #
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 subsl=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-DOMAIN=mantapxsl.my.id
-SUB_DOMAIN=onichan-${sub}.mantapxsl.my.id
-NS_DOMAIN=zerosl-${sub}.mantapxsl.my.id
-CF_ID=slinfinity69@gmail.com
-CF_KEY=dd2c5e0313f122b3c1833471d469b1025f492
+DOMAIN=lukavpn.live
+SUB_DOMAIN=onichan-${sub}.lukavpn.live
+NS_DOMAIN=zerosl-${sub}.lukavpn.live
+CF_ID=kayroy46@gmail.com
+CF_KEY=adb54ad8ec098de14c97f5f27ee8b1c81519a
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${SUB_DOMAIN}..."
