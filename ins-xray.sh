a17683d2be55f56848001333d97cac512915e872
bracoli <108661415+bracoli@users.noreply.github.com>
2022-12-11 23:11:23 +0800

    Update ins-xray.sh

--- ins-xray.sh
+++ ins-xray.sh
@@ -96,9 +96,7 @@ sleep 2
 # Uuid Service
 uuid=$(cat /proc/sys/kernel/random/uuid)
 # INSTALL XRAY
-wget -c -P /etc/samvpn/xray/ "https://github.com/XTLS/Xray-core/releases/download/v1.6.1/Xray-linux-64.zip"
-unzip -o /etc/samvpn/xray/Xray-linux-64.zip -d /etc/samvpn/xray
-rm -rf /etc/samvpn/xray/Xray-linux-64.zip
+wget -c -P /etc/samvpn/xray/xray "https://github.com/dharak36/Xray-core/releases/download/v1.0.0/xray.linux.64bit"
 chmod 655 /etc/samvpn/xray/xray
 # XRay boot service
 cat <<EOF >/etc/systemd/system/xray.service
